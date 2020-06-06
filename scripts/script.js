dd4ec886559c999745f9a09812b75db92b62e868
Kostya Zhi <smart.index@gmail.com>
2020-06-06 17:48:55 +0300

    add js init cards

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,40 @@ let formName = document.querySelector('.form__name');
 let formJob = document.querySelector('.form__job');
 const closeBtn = document.querySelector('.form__close-btn');
 const submitBtn = document.querySelector('.form__submit');
+const initialCards = [
+  {
+    name: 'Шушары',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/shushari.jpg',
+    imgAlt: 'Вид на здание НИИ в Шушарах'
+  },
+  {
+    name: 'Севастополь',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/sevastopol.jpg',
+    imgAlt: 'Плавбасейн в бухте Севастополя'
+  },
+  {
+    name: 'Санкт-Петербург',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/saint-pete.jpg',
+    imgAlt: 'Дворцы барокко зодчего Росси'
+  },
+  {
+    name: 'Медео',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/medeo.jpg',
+    imgAlt: 'Стена стадиона в Медео'
+  },
+  {
+    name: 'Алма-Ата',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/alma-ata.jpg',
+    imgAlt: 'Алма-Атинские дома близнецы'
+  },
+  {
+    name: 'Мурино',
+    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/murino.jpg',
+    imgAlt: 'Муринские дома-панельки'
+  }
+];
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('#cards-list');
 
 //func definitions
 function openPopup(event) {
@@ -28,7 +62,22 @@ function saveProfile(event) {
   popup.classList.remove('popup_opened');
 }
 
+function addCard(name, imgLink, imgAlt) {
+  //TODO check if empty
+  const cardClone = cardTemplate.cloneNode(true);
+  cardClone.querySelector('.element__title').textContent = name;
+  cardClone.querySelector('.element__img').src = imgLink;
+  cardClone.querySelector('.element__img').alt = imgAlt;
+  cardsList.append(cardClone);
+}
+
 //hook events listeners
 editProfile.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
 submitBtn.addEventListener('click', saveProfile)
+
+//functions call
+//init cards
+initialCards.forEach(function (card, i, cards) {
+  addCard(cards[i].name,cards[i].imgLink,cards[i].imgAlt);
+});
