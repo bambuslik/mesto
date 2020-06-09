19ec28702916938fe1897060b1dc4d949d9a0085
Kostya Zhi <smart.index@gmail.com>
2020-06-10 00:17:07 +0300

    fix - img alt text color

--- scripts/script.js
+++ scripts/script.js
@@ -19,39 +19,6 @@ const formCardImg = document.querySelector('.input-card-img');
 
 const popupCloseBtns = document.querySelectorAll('.popup__close-btn');
 const fullImgPopup = document.querySelector('.popup_type_img');
-
-const initialCards = [
-  {
-    name: 'Шушары',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/shushari.jpg',
-    imgAlt: 'Вид на здание НИИ в Шушарах'
-  },
-  {
-    name: 'Севастополь',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/sevastopol.jpg',
-    imgAlt: 'Плавбасейн в бухте Севастополя'
-  },
-  {
-    name: 'Санкт-Петербург',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/saint-pete.jpg',
-    imgAlt: 'Дворцы барокко зодчего Росси'
-  },
-  {
-    name: 'Медео',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/medeo.jpg',
-    imgAlt: 'Стена стадиона в Медео'
-  },
-  {
-    name: 'Алма-Ата',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/alma-ata.jpg',
-    imgAlt: 'Алма-Атинские дома близнецы'
-  },
-  {
-    name: 'Мурино',
-    imgLink: 'https://raw.githubusercontent.com/bambuslik/mesto/master/images/murino.jpg',
-    imgAlt: 'Муринские дома-панельки'
-  }
-];
 const cardTemplate = document.querySelector('.card-template').content;
 const cardsList = document.querySelector('.elements');
 
@@ -131,6 +98,7 @@ function addCard(event) {
 //FUNC CALL
 //init cards
 placeCards(initialCards);
+
 
 //HOOK EVENT LISTENERS
 editProfilePopupOpenBtn.addEventListener('click', editProfilePopupShow);
