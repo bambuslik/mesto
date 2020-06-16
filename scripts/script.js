fffc9a7465d7ec9ae9fe2e36225d870d0650754d
Kostya Zhi <smart.index@gmail.com>
2020-06-16 18:56:15 +0300

    add ability to close popup by esc and click on overlay

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,6 @@ const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__subtitle');
 const formName = document.querySelector('.input-profile-name');
 const formJob = document.querySelector('.input-profile-job');
-// const editProfileSubmitBtn = document.querySelector('.popup_type_profile .form__submit');
 const formProfile = document.querySelector('.form_type_profile');
 //edit profile --/
 
@@ -24,21 +23,16 @@ const cardTemplate = document.querySelector('.card-template').content;
 const cardsList = document.querySelector('.elements');
 
 //FUNC DEFINITIONS
-// function editProfilePopupShow() {
-// editProfilePopup.classList.add('popup_show');
-// formName.value = profileName.textContent;
-// formJob.value = profileJob.textContent;
-// }
-//
-// function addCardPopupShow() {
-//   addCardPopup.classList.add('popup_show');
-// }
+function popupHide() {
+  document.querySelector('.popup_show').classList.remove('popup_show');
+}
 
-// function showFullImg(event) {
-//   document.querySelector('.popup__img').src = event.target.src;
-//   document.querySelector('.popup__img-title').textContent = event.target.closest('.element').querySelector('.element__title').textContent;
-//   fullImgPopup.classList.add('popup_show');
-// }
+function popupHideOverlay(event) {
+  //check if click on overlay
+  if (event.target.classList.contains('popup')) {
+    popupHide();
+  }
+}
 
 function openPopup(popupName) {
   switch (popupName) {
@@ -46,21 +40,31 @@ function openPopup(popupName) {
       editProfilePopup.classList.add('popup_show');
       formName.value = profileName.textContent;
       formJob.value = profileJob.textContent;
+      editProfilePopup.addEventListener('click', popupHideOverlay);
       break;
+
     case 'card':
       addCardPopup.classList.add('popup_show');
+      addCardPopup.addEventListener('click', popupHideOverlay);
       break;
+
     case 'img':
       document.querySelector('.popup__img').src = event.target.src;
       document.querySelector('.popup__img-title').textContent = event.target.closest('.element').querySelector('.element__title').textContent;
       fullImgPopup.classList.add('popup_show');
+      fullImgPopup.addEventListener('click', popupHideOverlay);
       break;
   }
+
+  //close popup by ESC
+  document.addEventListener('keydown', (event) => {
+    if (event.keyCode === 27) {
+      popupHide();
+    }
+  });
+
 }
 
-function popupHide(event) {
-  event.target.closest('.popup').classList.remove('popup_show');
-}
 
 function saveProfile(event) {
   event.preventDefault();
@@ -77,8 +81,8 @@ function placeCards(cardsArray) {
   cardsArray.forEach(function (card, i, cards) {
     const cardClone = cardTemplate.cloneNode(true);
     cardClone.querySelector('.element__title').textContent = card.name;
-    cardClone.querySelector('.element__img').src = cards[i].imgLink;
-    cardClone.querySelector('.element__img').alt = cards[i].imgAlt;
+    cardClone.querySelector('.element__img').src = card.imgLink;
+    cardClone.querySelector('.element__img').alt = card.imgAlt;
 
     //не красиво внутри перебора вешать обработчик, но по-другому не понял как повесить, чтобы он видел карточку из template --\
     cardClone.querySelector('.element__like-btn').addEventListener('click', event => {
@@ -109,6 +113,7 @@ function addCard(event) {
   formCardTitle.value = '';
   formCardImg.value = '';
 }
+
 
 //FUNC CALL
 //init cards
