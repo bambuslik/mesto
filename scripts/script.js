570841aaf0ab37a91dbc9ae62ad4a95611a083a9
Kostya Zhi <smart.index@gmail.com>
2020-06-20 15:38:55 +0300

    add function addcards refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -13,6 +13,7 @@ const addCardPopupOpenButton = document.querySelector('.profile__add-btn');
 const formCard = document.querySelector('.form_type_card');
 const formCardTitle = document.querySelector('.input-card-title');
 const formCardImg = document.querySelector('.input-card-img');
+const formCardImgAlt = 'Извините, но эту красоту невозможно описать словами';
 //add card --/
 
 const popupElements = document.querySelectorAll('.popup');
@@ -44,7 +45,6 @@ function popupHideEsc(event) {
 
 function openPopup(popupClass) {
   document.querySelector(popupClass).classList.add('popup_show');
-
   //close popup by ESC
   document.addEventListener('keydown', popupHideEsc);
 }
@@ -60,54 +60,55 @@ function cardLike(event) {
   event.target.classList.toggle('element__like-btn_status_active');
 }
 
-function createCard() {
-
+function cardRemove(event) {
+  event.target.closest('.element').remove();
 }
 
-//я так и не понял почему placeCards это плохое имя для функции, дословный перевод "разместить карточки"
-// >>А для этого стоит добавить глагол - place это и есть глагол
-function placeCards(cardsArray) {
-  cardsArray.forEach(function (card, i, cards) {
-    const cardClone = cardTemplate.cloneNode(true);
-    cardClone.querySelector('.element__title').textContent = card.name;
-    cardClone.querySelector('.element__img').src = card.imgLink;
-    cardClone.querySelector('.element__img').alt = card.imgAlt;
+function cardPopupInit(event) {
+  document.querySelector('.popup__img').src = event.target.src;
+  document.querySelector('.popup__img-title').textContent = event.target.closest('.element').querySelector('.element__title').textContent;
+  openPopup('.popup_type_img');
+}
 
-    //не красиво внутри перебора вешать обработчик, но по-другому не понял как повесить, чтобы он видел карточку из template --\
-    cardClone.querySelector('.element__like-btn').addEventListener('click', event => {
-      cardLike(event);
-    });
-    cardClone.querySelector('.element__trash').addEventListener('click', event => {
-      event.target.closest('.element').remove();
-    });
-    cardClone.querySelector('.element__img').addEventListener('click', function () {
-      document.querySelector('.popup__img').src = event.target.src;
-      document.querySelector('.popup__img-title').textContent = event.target.closest('.element').querySelector('.element__title').textContent;
-      openPopup('.popup_type_img');
-    });
+function createCard(cardTitle, cardImgLink, cardImgAlt) {
+  const cardClone = cardTemplate.cloneNode(true);
+  cardClone.querySelector('.element__title').textContent = cardTitle;
+  cardClone.querySelector('.element__img').src = cardImgLink;
+  cardClone.querySelector('.element__img').alt = cardImgAlt;
+  cardClone.querySelector('.element__like-btn').addEventListener('click', event => {
+    cardLike(event);
+  });
+  cardClone.querySelector('.element__trash').addEventListener('click', event => {
+    cardRemove(event);
+  });
+  cardClone.querySelector('.element__img').addEventListener('click', event => {
+    cardPopupInit(event);
+  });
+  return cardClone;
+}
 
-    cardsList.prepend(cardClone);
+function addCard(cartToAdd) {
+  cardsList.prepend(cartToAdd);
+}
+
+function initCards(initCards) {
+  initCards.forEach(card => {
+    const cartToAdd = createCard(card.name, card.imgLink, card.imgAlt);
+    addCard(cartToAdd);
   });
 }
 
-function addCard(event) {
+function submitAddCardForm(event) {
   event.preventDefault();
-  const cardToAdd = [
-    {
-      name: formCardTitle.value,
-      imgLink: formCardImg.value,
-      imgAlt: 'Извините, но эту красоту невозможно описать словами'
-    }
-  ];
-  placeCards(cardToAdd);
+  const cartToAdd = createCard(formCardTitle.value, formCardImg.value, formCardImgAlt);
+  addCard(cartToAdd);
   popupHide();
   formCardTitle.value = '';
   formCardImg.value = '';
 }
 
 //FUNC CALL
-//init cards
-placeCards(initialCards);
+initCards(initialCards);
 
 //HOOK EVENT LISTENERS
 editProfilePopupOpenButton.addEventListener('click', () => {
@@ -120,7 +121,7 @@ addCardPopupOpenButton.addEventListener('click', () => {
 });
 
 formProfile.addEventListener('submit', saveProfile);
-formCard.addEventListener('submit', addCard);
+formCard.addEventListener('submit', submitAddCardForm);
 
 popupCloseButtons.forEach(function (popupCloseButton) {
   popupCloseButton.addEventListener('click', function () {
