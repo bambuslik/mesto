85c0705fe86a7d599df4c78304b043fefd97477e
Kostya Zhi <smart.index@gmail.com>
2020-09-14 02:00:25 +0300

    fix initial images

--- src/index.js
+++ src/index.js
@@ -1,2 +1,169 @@
 import './pages/index.css';
-import avatarImage from './images/avatar.jpg';
+
+//IMPORT
+import {Card} from './components/Card.js';
+import {FormValidator} from "./components/FormValidator.js";
+import Section from "./components/Section.js";
+import PopupWithImage from "./components/PopupWithImage.js";
+import PopupWithForm from "./components/PopupWithForm.js";
+import UserInfo from "./components/UserInfo.js";
+
+//LET, CONST DEFINITIONS
+const initialCards = [
+  {
+    name: 'Шушары',
+    imgLink: 'http://webpunk.ru/images/shushari.jpg',
+    imgAlt: 'Вид на здание НИИ в Шушарах'
+  },
+  {
+    name: 'Севастополь',
+    imgLink: 'http://webpunk.ru/images/sevastopol.jpg',
+    imgAlt: 'Плавбасейн в бухте Севастополя'
+  },
+  {
+    name: 'Санкт-Петербург',
+    imgLink: 'http://webpunk.ru/images/saint-pete.jpg',
+    imgAlt: 'Дворцы барокко зодчего Росси'
+  },
+  {
+    name: 'Медео',
+    imgLink: 'http://webpunk.ru/images/medeo.jpg',
+    imgAlt: 'Стена стадиона в Медео'
+  },
+  {
+    name: 'Алма-Ата',
+    imgLink: 'http://webpunk.ru/images/alma-ata.jpg',
+    imgAlt: 'Алма-Атинские дома близнецы'
+  },
+  {
+    name: 'Мурино',
+    imgLink: 'http://webpunk.ru/images/murino.jpg',
+    imgAlt: 'Муринские дома-панельки'
+  }
+];
+
+//edit profile --\
+const profileEditPopupOpenButton = document.querySelector('.profile__edit-btn');
+const profileFormName = document.querySelector('.input-profile-name');
+const profileFormJob = document.querySelector('.input-profile-job');
+const formProfile = document.querySelector('.form_type_profile');
+//edit profile --/
+
+//add card --\
+const addCardPopupOpenButton = document.querySelector('.profile__add-btn');
+const formCard = document.querySelector('.form_type_card');
+const formCardImgAlt = 'Извините, но эту красоту невозможно описать словами';
+const listElement = '.elements';
+//add card --/
+
+//VALIDATION
+const validatorConfig = {
+  inputErrorClass: 'form__input-text_type_error',
+  errorClass: 'form__input-error_type_visible',
+  inactiveButtonClass: 'form__submit_disabled',
+  submitButtonSelector: '.form__submit',
+  submitButtonDisabledClass: '.form__submit_disabled'
+}
+
+const formProfileValidator = new FormValidator(validatorConfig, formProfile);
+formProfileValidator.enableValidation();
+
+const formNewCardValidator = new FormValidator(validatorConfig, formCard);
+formNewCardValidator.enableValidation();
+
+const userInfo = new UserInfo(
+  {
+    userNameSelector: '.profile__title',
+    userJobSelector: '.profile__subtitle'
+  }
+);
+
+const imagePopup = new PopupWithImage('.popup_type_img');
+imagePopup.setEventListeners();
+
+const cardsList = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const card = new Card(
+        item.name,
+        item.imgLink,
+        item.imgAlt,
+        '.card-template',
+        {
+          handleCardClick: (event) => {
+            imagePopup.open(event);
+          }
+        }
+      );
+      cardsList.addItem(card.generateCard());
+    }
+  },
+  listElement
+);
+
+cardsList.renderItems();
+
+const addCardPopup = new PopupWithForm(
+  '.popup_type_card',
+  {
+    submitCallback: (item) => {
+      const cardItem = [
+        {
+          name: item.title,
+          imgLink: item.imgLink
+        }
+      ];
+
+      const cardsList = new Section(
+        {
+          items: cardItem,
+          renderer: (item) => {
+            const card = new Card(
+              item.name,
+              item.imgLink,
+              item.imgAlt,
+              '.card-template',
+              {
+                handleCardClick: (event) => {
+                  imagePopup.open(event);
+                }
+              }
+            );
+            cardsList.addItem(card.generateCard());
+            addCardPopup.close();
+          }
+        },
+        listElement
+      );
+      cardsList.renderItems();
+    }
+  }
+);
+
+addCardPopup.setEventListeners();
+
+addCardPopupOpenButton.addEventListener('click', () => {
+  formNewCardValidator.resetFormByCloseModal();
+  addCardPopup.open();
+});
+
+const editProfilePopup = new PopupWithForm(
+  '.popup_type_profile',
+  {
+    submitCallback: (item) => {
+      userInfo.setUserInfo(item.name, item.job);
+      editProfilePopup.close();
+    }
+  }
+);
+
+editProfilePopup.setEventListeners();
+
+profileEditPopupOpenButton.addEventListener('click', () => {
+  formProfileValidator.resetFormByCloseModal();
+  const userInfoData = userInfo.getUserInfo();
+  profileFormName.value = userInfoData.name;
+  profileFormJob.value = userInfoData.job;
+  editProfilePopup.open();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,103 +1,47 @@
-module.exports = env => {
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-  //определям mode исходя из значения из package.json
-  const mode = env.MODE;
-  const isDev = env.MODE === 'dev';
-  const isBuild = env.MODE === 'build';
 
-  //подключаем плагины
-  const path = require('path');
-  const HtmlWebpackPlugin = require('html-webpack-plugin');
-  const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-  const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-  const TerserWebpackPlugin = require('terser-webpack-plugin');
-
-  const sourceMap = (isDev) ? 'source-map' : 'none';
-  const htmlCollapseWhitespace = (isDev) ? false : true;
-
-  return {
-    //точка входа
-    entry: {
-      //в проде нужен бабел?
-      main: ['@babel/polyfill', './src/index.js'],
-    },
-    //выход кода в:
-    output: {
-      filename: 'js/[name].bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    optimization: {
-      //объеденить дубли зависимостей, если они подключены в разных чанках
-      splitChunks: {
-        chunks: 'all'
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff2)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            // добавьте объект options
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader'
+        ]
       }
-    },
-    devServer: {
-      port: 4200,
-      // hot: isDev
-    },
-    //сорс мапа
-    devtool: sourceMap,
-    //инициализируем плагины
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        //прокинуть переменную в html
-        templateParameters: {
-          mode: mode,
-        },
-        minify: {
-          //сжать html
-          collapseWhitespace: htmlCollapseWhitespace
-        }
-      }),
-      new CleanWebpackPlugin({
-        //лог в консоль
-        verbose: true,
-      }),
-      new MiniCssExtractPlugin({
-        filename: './styles/[name].css',
-      }),
-      //TODO переписать с условием от mode чтобы не ставить руками
-      //new OptimizeCssAssetsPlugin(), //только в прод
-      //new TerserWebpackPlugin() //только в прод
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                //в дев
-                // hmr: true,
-                // reloadAll: true
-              }
-            },
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-              ]
-            }
-          }
-        },
-        {
-          test: /\.(png|svg|jpg|gif|woff2)$/,
-          exclude: /node_modules/,
-          loader: 'file-loader'
-        }
-
-      ]
-    }
-  }
-}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
+};
